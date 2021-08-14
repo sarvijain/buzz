3ffec5dfbfa543c67dc0f48c8522015b85aa5669
sarvi jain <jainaksp2018@gmail.com>
2021-08-14 10:59:07 +0530

    hii

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Facebook from './screens/fb';
+import Instagram from './screens/ig';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{backgroundColor:"pink",width:600,textAlign:"center",fontSize:30,alignItems:"center",alignSelf:"center"}}>BUZZ</Text>
+     <AppContainer/>
     </View>
   );
 }
+const appNavigator = createBottomTabNavigator({
+  Instagram:{screen: Instagram},
+  Facebook:{screen: Facebook}
 
+})
+
+const AppContainer=createAppContainer(
+  appNavigator
+)
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+   
   },
 });
---
+++ screens/fb.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class Facebook extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text style={{color:"red", fontFamily:"serif",fontSize:20,textAlign:"center",marginTop:250}}>facebook</Text>
+            </View>
+        )
+    }
+    }
